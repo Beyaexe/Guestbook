e8f2e2ab73048a73f0b7f57b954346150bccb6f7
beya.exe <jonatas1096@hotmail.com>
2026-07-16 19:13:35 -0300

    form with sendername and send message (submit)

--- guestbook-web/src/App.tsx
+++ guestbook-web/src/App.tsx
@@ -5,6 +5,8 @@ import { questionsList } from "./models/questions"
 function App() {
 
   const [currentQuestion, setCurrentQuestion] = useState<Question | null>(null)
+  const [answerText, setAnswerText] = useState("")
+  const [senderName, setSenderName] = useState("");
 
   function handleSort() {
 
@@ -18,18 +20,59 @@ function App() {
     <div>
       <h1>Grimoire of Messages 🧙‍♂️</h1>
 
-
       {currentQuestion ? (
+        <div>
           <div>
             <p><strong>✨{currentQuestion.id}: </strong>{currentQuestion.text}</p>
+            <button onClick={handleSort}>Sortear Pergunta 🔮</button>
           </div>
-        ) : 
-        (
-          <p>Clique no botão para invocar uma pergunta do grimório...</p>
-        )
-      }
 
-      <button onClick={handleSort}>Sortear Pergunta 🔮</button>
+          <form onSubmit={(e) => {
+            e.preventDefault(); // Evita que a página recarregue
+            alert(`Enviando...`);
+          }}>
+
+            {/* Nome */}
+            <div>
+              <label htmlFor="nameInput">Quem é você? </label>
+              <input
+                id="nameInput"
+                type="text"
+                placeholder="Herbert Richards"
+                value={senderName}
+                onChange={(e) => setSenderName(e.target.value)}
+                required
+              />
+            </div>
+
+            {/* Caixa de texto para da resposta */}
+            <div>
+              <textarea
+                placeholder="Digite sua resposta mágica aqui..."
+                value={answerText}
+                onChange={(e) => setAnswerText(e.target.value)}
+                rows={4}
+                cols={40}
+                required
+                maxLength={500}
+              />
+            </div>
+
+            {/* O Enviar */}
+            <button type="submit">
+              Enviar Mensagem 📜
+            </button>
+
+
+          </form>
+        </div>
+      ) : (
+        <>
+          <p>Clique no botão para invocar uma pergunta do grimório...</p>
+          <button onClick={handleSort}>Sortear Pergunta 🔮</button>
+        </>
+      )}
+
     </div>
   )
 }
