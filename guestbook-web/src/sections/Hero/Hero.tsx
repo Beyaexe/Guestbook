84dba6a030b833569c7478e21ac2bdb584ce5b9c
beya.exe <jonatas1096@hotmail.com>
2026-07-24 15:34:07 -0300

    add first complete hero view

--- guestbook-web/src/sections/Hero/Hero.tsx
+++ guestbook-web/src/sections/Hero/Hero.tsx
@@ -13,7 +13,7 @@ export function HeroSection() {
 
             <div className="hero-content">
                 <p className="hero-quote">
-                    "Este grimório permaneceu perdido por séculos. Dizem que cada viajante que registra seu nome deixa um fragmento de si gravado para sempre.
+                    "Este grimório permaneceu perdido por séculos. Dizem que cada viajante que nele escreve deixa também um fragmento de si gravado para sempre.
                     Se chegou até aqui, talvez o livro também tenha escolhido você."
                 </p>
                 <span className="hero-author">— Dos céus</span>
@@ -40,18 +40,41 @@ export function HeroSection() {
             <div className="clouds-wrapper">
                 {/* Nuvem do fundo (mais suave e lenta) */}
                 <img
-                    src="./src/assets/images/cloud.png"
+                    src="./src/assets/images/cloudRemake3.png"
                     alt="Nuvem Traseira"
                     className="cloud-layer cloud-back"
                 />
 
+
+                <div className="crusader-knight-wrapper">
+                    <img
+                        src="./src/assets/images/crusader-knight.png"
+                        alt="Crusader Knight"
+                        className="crusader-knight"
+                    />
+                </div>
+
+                <div className="saint-maria-wrapper">
+                    <img
+                        src="./src/assets/images/saint-maria.png"
+                        alt="Saint Maria"
+                        className="saint-maria"
+                    />
+                </div>
+
+
+
                 {/* Nuvem da frente (espelhada e mais rápida) */}
                 <img
-                    src="./src/assets/images/cloud.png"
+                    src="./src/assets/images/cloudRemake3.png"
                     alt="Nuvem Frontal"
                     className="cloud-layer cloud-front"
                 />
             </div>
+
+
+
+
 
         </section>
     )
